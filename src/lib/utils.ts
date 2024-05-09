d65f9c89f8a90224cd026de3b48e35e80144859c
Alon Mlievski <alonmlievski@gmail.com>
2024-05-09 13:04:33 +0300

    Util functions added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-export const utils = { generateRandomId }
+export const utils = { generateRandomId, priceFromHigh52w, generateStock }
 
 function generateRandomId(): string {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -9,3 +9,17 @@ function generateRandomId(): string {
     return id;
 }
 
+function priceFromHigh52w(price: number, high_52w: number) {
+    return ((price / high_52w) * 100).toFixed(2)
+}
+
+
+function generateStock(stockName: string) {
+    return {
+        ticker: stockName.substring(0, 3).toUpperCase(),
+        name: stockName,
+        price: Math.random() * 100,
+        high_52w: Math.random() * 100,
+        id: generateRandomId()
+    }
+}
